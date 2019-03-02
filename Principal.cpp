8a4d40ef030ab81e733711a6216451176a95adb6
jgarciapft <jgarciapft@alumnos.unex.es>
2019-03-02 17:00:43 +0100

    Archivos:     - Principal.cpp/.h. Implementada la primera versión del punto de entrada al programa

--- Principal.cpp
+++ Principal.cpp
@@ -1,6 +1,44 @@
+#include "Principal.h"
 
+const LPCSTR CONSTANTES::COM[] = {"COM1", "COM2", "COM3", "COM4"};
+const char CONSTANTES::MSJ_SEL_COM[] = "Seleccionar el puerto a utilizar:\n1. COM1\n2. COM2\n3. COM3\n4. COM4";
+const char CONSTANTES::MSJ_ERROR_SEL_COM[] = "Puerto COM incorrecto";
+const DWORD CONSTANTES::VELOCIDAD_DEF = 9600;
+const BYTE CONSTANTES::NUM_BITS_X_BYTE_DEF = 8;
+const BYTE CONSTANTES::PARIDAD_DEF = 0;
+const BYTE CONSTANTES::BITS_STOP_DEF = 0;
 
 int main() {
-	// TODO - implement Principal::main
-	throw "Not yet implemented";
+
+	/** INICIALIZACIÓN DEL PUERTO COM */
+
+	CargadorManejadorPuertoCOM cargadorPCOM = CargadorManejadorPuertoCOM();
+	cargadorPCOM.setNombrePuerto(CONSTANTES::COM[ManejadorEntradaUsuario::preguntarRespEntRang(CONSTANTES::MSJ_SEL_COM,
+																							   CONSTANTES::MSJ_ERROR_SEL_COM,
+																							   1,
+																							   sizeof(CONSTANTES::COM) /
+																							   sizeof(LPCSTR)) - 1]);
+	cargadorPCOM.setVelocidad(CONSTANTES::VELOCIDAD_DEF);
+	cargadorPCOM.setNumBitsXByte(CONSTANTES::NUM_BITS_X_BYTE_DEF);
+	cargadorPCOM.setParidad(CONSTANTES::PARIDAD_DEF);
+	cargadorPCOM.setBitsParada(CONSTANTES::BITS_STOP_DEF);
+
+	cargadorPCOM.inicializarManejador(); // Intenta inicializar el puerto COM con los parámetros elegidos
+
+	/** MANEJO DE LAS OPERACIONES DEL PUERTO COM */
+
+	// Manejador de lectura del puerto COM
+	LectorPuertoCOM lectorPuertoCOM = LectorPuertoCOM(ManejadorPuertoCOM::recuperarInstancia());
+	// Manejador de escritura del puerto COM
+	EscritorPuertoCOM escritorPuertoCOM = EscritorPuertoCOM(ManejadorPuertoCOM::recuperarInstancia());
+
+	while (!escritorPuertoCOM.getFinCaracter()) { // Mientras el usuario no pulse la tecla ESCAPE
+		lectorPuertoCOM.lectura(); // Lectura del puerto COM
+		if (kbhit()) // Si se ha pulsado una tecla invoca al manejador de escritura del puerto COM
+			escritorPuertoCOM.escritura();
+	}
+
+	/** CIERRE DEL PUERTO COM */
+
+	ManejadorPuertoCOM::recuperarInstancia()->cerrarManejador();
 }
--- Principal.h
+++ Principal.h
@@ -1,16 +1,49 @@
 #ifndef PRINCIPAL_H
 #define PRINCIPAL_H
 
+#include "ManejadorPuertoCOM.h"
+#include "ManejadorEntradaUsuario.h"
 #include "CargadorManejadorPuertoCOM.h"
-#include "EscritorPuertoCOM.h"
 #include "LectorPuertoCOM.h"
-#include "ManejadorSalida.h"
+#include "EscritorPuertoCOM.h"
 
 /**
- * Punto de entrada al programa. Controla el flujo de ejecución
+ * Punto de entrada al programa. Controla el flujo de ejecución y contiene la colección de constantes comunes al programa
  * 
  * @author Juan Pablo García Plaza Pérez @ 2ºGIIIS/GrupoA
  * @author José Ángel Concha Carrasco @ 2ºGIIIS/GrupoA
  */
+
+class CONSTANTES {
+public:
+	/**
+	 * Colección de constantes de cadenas puerto COM
+	 */
+	static const LPCSTR COM[];
+	/**
+	 * Mensaje para mostrar las opciones de seleccionar un puerto COM
+	 */
+	static const char MSJ_SEL_COM[];
+	/**
+	 * Mensaje para indicar opción inválida en la elección de puerto COM
+	 */
+	static const char MSJ_ERROR_SEL_COM[];
+	/**
+	 * Tasa de baudios por defecto
+	 */
+	static const DWORD VELOCIDAD_DEF;
+	/**
+	 * Número de bits por byte enviado / recibido por defecto
+	 */
+	static const BYTE NUM_BITS_X_BYTE_DEF;
+	/**
+	 * Configuración de paridad por defecto
+	 */
+	static const BYTE PARIDAD_DEF;
+	/**
+	 * Configuración de bits de stop por defecto
+	 */
+	static const BYTE BITS_STOP_DEF;
+};
 
 #endif
