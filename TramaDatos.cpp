f1c3439bf5f77cc48e66bcef9367bef322f73828
jgarciapft <jgarciapft@alumnos.unex.es>
2019-03-06 09:35:26 +0100

    Archivos:     - Añadido la nueva clase 'TramaDatos'     - Actualizado el diagrama UML

---
+++ TramaDatos.cpp
@@ -0,0 +1,48 @@
+#include "TramaDatos.h"
+
+TramaDatos::TramaDatos() {
+	// TODO - implement TramaDatos::TramaDatos
+	throw "Not yet implemented";
+}
+
+void TramaDatos::Trama(unsigned char S, unsigned char D, unsigned char C, unsigned char NT, unsigned char L,
+					   unsigned char *datos) {
+	// TODO - implement TramaDatos::Trama
+	throw "Not yet implemented";
+}
+
+unsigned char TramaDatos::getL() {
+	// TODO - implement TramaDatos::getL
+	throw "Not yet implemented";
+}
+
+unsigned char TramaDatos::getBCE() {
+	// TODO - implement TramaDatos::getBCE
+	throw "Not yet implemented";
+}
+
+void TramaDatos::setL(/* Nuevo valor de la longitud */unsigned char L) {
+	// TODO - implement TramaDatos::setL
+	throw "Not yet implemented";
+}
+
+void TramaDatos::setBCE(/* Nuevo valor del BCE */unsigned char BCE) {
+	// TODO - implement TramaDatos::setBCE
+	throw "Not yet implemented";
+}
+
+void TramaDatos::calcularBCE() {
+	// TODO - implement TramaDatos::calcularBCE
+	throw "Not yet implemented";
+}
+
+void TramaDatos::setAttr(unsigned char S, unsigned char D, unsigned char C, unsigned char NT, unsigned char L,
+						 unsigned char *datos) {
+	// TODO - implement TramaDatos::setAttr
+	throw "Not yet implemented";
+}
+
+std::string TramaDatos::toString() {
+	// TODO - implement TramaDatos::toString
+	throw "Not yet implemented";
+}
---
+++ TramaDatos.h
@@ -0,0 +1,77 @@
+#ifndef TRAMADATOS_H
+#define TRAMADATOS_H
+
+#include "Trama.h"
+
+/**
+ * Clase que modela una trama de datos
+ * 
+ * @author Juan Pablo García Plaza Pérez @ 2ºGIIIS/GrupoA
+ * @author José Ángel Concha Carrasco @ 2ºGIIIS/GrupoA
+ */
+class TramaDatos : Trama {
+
+private:
+	/**
+	 * Longitud del mensaje contenido en la trama
+	 */
+	unsigned char L;
+	/**
+	 * Mensaje contenido en la trama
+	 */
+	unsigned char *datos;
+	/**
+	 * Bloque de control de errores
+	 */
+	unsigned char BCE;
+
+public:
+	TramaDatos();
+
+	void
+	Trama(unsigned char S, unsigned char D, unsigned char C, unsigned char NT, unsigned char L, unsigned char *datos);
+
+	/**
+	 * Método accesor del atributo 'L'
+	 */
+	unsigned char getL();
+
+	/**
+	 * Método accesor del atributo 'BCE'
+	 */
+	unsigned char getBCE();
+
+private:
+	/**
+	 * Método modificador del atributo 'L'
+	 */
+	void setL(/* Nuevo valor de la longitud */unsigned char L);
+
+	/**
+	 * Método modificador del atributo 'BCE'
+	 */
+	void setBCE(/* Nuevo valor del BCE */unsigned char BCE);
+
+	/**
+	 * Calcula el BCE para el mensaje contenido en la trama
+	 */
+	void calcularBCE();
+
+public:
+	/**
+	 * Método modificador global
+	 */
+	void
+	setAttr(/* Nuevo valor del caracter de sincronismo */unsigned char S,/* Nuevo valor del caracter de dirección */
+														 unsigned char D,/* Nuevo valor del caracter de control */
+														 unsigned char C, /* Nuevo valor del número de trama */
+														 unsigned char NT,/* Nuevo valor de la longitud */
+														 unsigned char L, /* Nuevo mensaje */unsigned char *datos);
+
+	/**
+	 * Devuelve la representación en cadena de la trama de datos
+	 */
+	std::string toString();
+};
+
+#endif
